accf01044872bcf341b8db06208224aab2f27375
David  on Go <davidsomerville@comcast.net>
2019-12-06 13:41:12 +1300

    updated import for  bing to make it easier to swap from uping  to the uping_  test libraries Also changed maxSize of the ping for the bing routine to be paramitized

--- src/probe/ubing.py
+++ src/probe/ubing.py
@@ -2,14 +2,14 @@
 # copyright (c) 2019 David Somerville <david.a.somerville@outlook.com>
 # License: MIT
 
-import uping_
+import uping_ as uping
 
 
-def getLowestPing(host, samples, size, timeout=5000, quiet=True):
+def getLowestPing(host, samples, size, timeout=5000, quiet=False):
     # Make a list of ping results
     pings = []
     for x in range(samples):
-        pings.append(uping_.ping(host, size, timeout))
+        pings.append(uping.ping(host, size, timeout))
     # Review results for number of successful pings and get the lowest latency
     minPing = 9999
     failCnt = 0
@@ -27,7 +27,7 @@ def getLowestPing(host, samples, size, timeout=5000, quiet=True):
         return minPing
 
 
-def bing(host, samples=3, timeout=5000, quiet=False):
+def bing(host, samples=3, maxSize=1460, timeout=5000, quiet=False):
     # perform required number of ping samples to the host using 16byte and maxsize packets
     # also get esp32 overhead time for the same pings to localhost (no network times)
     # calculate and return bandwidth (bps) and latency (ms) based on the ping samples
@@ -37,51 +37,51 @@ def bing(host, samples=3, timeout=5000, quiet=False):
     latency = getLowestPing(host, samples, 16, timeout)
     # Get Lowest loopback latencies
     loopback26 = getLowestPing("127.0.0.1", samples, 26, timeout)
-    loopback1480 = getLowestPing("127.0.0.1", samples, 1480, timeout)
+    loopbackMax = getLowestPing("127.0.0.1", samples, maxSize, timeout)
     # Get Lowest target latencies
     target26 = getLowestPing(host, samples, 26, timeout)
-    target1480 = getLowestPing(host, samples, 1480, timeout)
+    targetMax = getLowestPing(host, samples, maxSize, timeout)
 
     # Check Results before calculating
 
     if (loopback26 == None):
         not quiet and print("getlowestPing failed: loopback26 == None")
         return None
-    if (loopback1480 == None):
-        not quiet and print("getlowestPing failed: loopback1480 == None")
+    if (loopbackMax == None):
+        not quiet and print("getlowestPing failed: loopbackMax == None")
         return None
     if (target26 == None):
         not quiet and print("getlowestPing failed: target26 == None")
         return None
-    if (target1480 == None):
-        not quiet and print("getlowestPing failed: target1480 == None")
+    if (targetMax == None):
+        not quiet and print("getlowestPing failed: targetMax == None")
         return None
     if (latency == None):
         not quiet and print("getlowestPing failed: latency == None")
         return None
-    if (loopback26 > loopback1480):
+    if (loopback26 > loopbackMax):
         not quiet and print(
-            "bing calculation not possable: loopback26 > loopback1480")
+            "bing calculation not possable: loopback26 > loopbackMax")
         return None
-    if (target26 > target1480):
+    if (target26 > targetMax):
         not quiet and print(
-            "bing calculation not possable: target26 > target1480")
+            "bing calculation not possable: target26 > targetMax")
         return None
     if (loopback26 > target26):
         not quiet and print(
             "bing calculation not possable: loopback26 > target26")
         return None
-    if (loopback1480 > target1480):
+    if (loopbackMax > targetMax):
         not quiet and print(
-            "bing calculation not possable: loopback1480 > target1480")
+            "bing calculation not possable: loopbackMax > targetMax")
         return None
-    deltaLatency = ((target1480 - loopback1480) - (target26 - loopback26))
+    deltaLatency = ((targetMax - loopbackMax) - (target26 - loopback26))
     if (deltaLatency <= 0):
         not quiet and print(
             "bing calculation not possable: deltaLatency <= 0")
         return None
     not quiet and print("deltaLatency:", deltaLatency)
-    deltaPayloadBits = (1480-26) * 8
+    deltaPayloadBits = (maxSize-26) * 8
     not quiet and print("deltaPayloadBits:", deltaPayloadBits)
     bps = int((1000/(deltaLatency)) * deltaPayloadBits * 2)
     not quiet and print("bps:", bps)
--- src/probe/uping_.py
+++ src/probe/uping_.py
@@ -1,5 +1,6 @@
 # Stubbed out ping to test bing without network or esp32
-# bing should return 1000/(50-10) * 1454 * 8 * 2 = 581,600 bps
+# bing should return 1000/(50-10) * 1434 * 8 * 2 = 573,600 bps
+# assuming the default max ping size of 1460 bytes
 
 
 def ping(host, size=16, timeout=5000):
@@ -9,10 +10,10 @@ def ping(host, size=16, timeout=5000):
         elif (size == 26):
             return 5, 64, 60
         else:
-            return 10, 64, 1514
+            return 10, 64, 1494
     if (size == 16):
         return 10, 64, 60
     elif (size == 26):
         return 15, 64, 60
     else:
-        return 60, 64, 1514
+        return 60, 64, 1494
