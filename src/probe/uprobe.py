826c49e18ff57dbcc09b161bf55b7f2d08fad00e
David  on Go <davidsomerville@comcast.net>
2019-12-28 15:07:38 -0500

    Updated name of utests to uprobes and got it working

---
+++ src/probe/uprobe.py
@@ -0,0 +1,71 @@
+# Encapsulate IO operations for working with probe files
+# valid operations are:
+# reset() - make empty /probes folder
+# add(fname, objProbe)
+# get()
+# remove(fname)
+# Note: do a os.chdir('/') before all operations to make sure we are in correct dir
+#  even if earlier operation fails
+import os
+import ujson
+
+
+def reset():
+    print("start of reset")
+    os.chdir('/')
+    if ('probes' in os.listdir()):
+        print("probes dir found, deleting any files")
+        for i in os.listdir('probes'):
+            print("removing", i)
+            os.remove('probes/' + i)
+    else:
+        print("probes dir not found, creating one")
+        os.mkdir('probes')
+    os.chdir('/')
+
+
+def add(fname, objProbe):
+    print("start of add")
+    os.chdir('/')
+    strJson = ujson.dumps(objProbe)
+    f = open('/probes/' + fname + '.json', 'wt')
+    l = f.write(strJson)
+    # must perform a flush() to make sure string in buffer is written to file
+    f.flush()
+    f.close
+    print("end of add, characters in file: ", l)
+
+
+def get(fname):
+    print("start of get")
+    os.chdir('/')
+    f = open('/probes/' + fname + '.json', 'rt')
+    v = f.read()
+    objJson = ujson.loads(v)
+    f.close
+    print("end of get")
+    return objJson
+
+
+def remove(fname):
+    print("start of remove")
+    os.chdir('/')
+    try:
+        os.remove('/probes/' + fname + '.json')
+    except:
+        print("File not found")
+    print("end of remove")
+
+
+myprobe = {
+    "id": "8484hf84f8hfh84bhflwld9h",
+    "type": 'bing',
+    'target': 'ourDars.com'
+}
+
+reset()
+add('probe2', myprobe)
+obj = get('probe2')
+print(obj)
+print("obj[id]", obj['id'])
+remove("probe2")
