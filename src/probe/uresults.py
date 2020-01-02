553e7292b316f65e0d1e0aca6c83258907c3b554
David <39265971+somervda@users.noreply.github.com>
2020-01-02 08:44:31 -0500

    wip added uresults

---
+++ src/probe/uresults.py
@@ -0,0 +1,94 @@
+# Encapsulate IO operations for working with result files
+# valid operations are:
+# reset() - make empty /results folder
+# add(fname, objResult) : Gets current time applied as file name
+# list() : returns a list of result files
+# get(fname) : returns object with contents of the fname results file
+# remove(fname)
+# Note: do a os.chdir('/') before all operations to make sure we are in correct dir
+#  even if earlier operation fails
+import os
+import ujson
+import utime
+
+
+def reset():
+    print("start of reset")
+    os.chdir('/')
+    if ('results' in os.listdir()):
+        print("results dir found, deleting any files")
+        for i in os.listdir('results'):
+            print("removing", i)
+            os.remove('results/' + i)
+    else:
+        print("results dir not found, creating one")
+        os.mkdir('results')
+    os.chdir('/')
+
+
+def add(objResults):
+    print("start of add")
+    os.chdir('/')
+    strJson = ujson.dumps(objResults)
+    # filename is based on esp32 time (note does not need to be real time
+    # and only needs to be unique relative to cpu lifecycle)
+    fname = "r" + str(utime.ticks_ms())
+    f = open('/results/' + fname + '.json', 'wt')
+    l = f.write(strJson)
+    # must perform a flush() to make sure string in buffer is written to file
+    f.flush()
+    f.close
+    print("end of add, characters in file: ", l)
+
+
+def list():
+    # return all the result files as names in a list
+    print("start of list")
+    os.chdir('/')
+    resultsList = []
+    if ('results' in os.listdir()):
+        resultsList = os.listdir('results')
+    print("end of list")
+    return resultsList
+
+
+def get(fname):
+    # return all the result files an object
+    print("start of get")
+    os.chdir('/')
+    result = {}
+    if ('results' in os.listdir()):
+        if (fname in os.listdir('results')):
+            f = open('/results/' + fname, 'rt')
+            v = f.read()
+            result = ujson.loads(v)
+            f.close
+    print("end of get")
+    return result
+
+
+def remove(fname):
+    print("start of remove")
+    os.chdir('/')
+    try:
+        os.remove('/results/' + fname)
+    except:
+        print("File not found")
+    print("end of remove")
+
+
+result = {
+    "operationId": "8484hf84f8hfh84bhflwld9h",
+    "operationTime": 38383812010,
+    "bps": 1000000,
+    'target': 'ourDars.com',
+    'rtl': 230
+}
+
+
+# reset()
+# add('probe1', myprobe)
+add(result)
+plist = list()
+print(plist)
+# remove("probe2")
