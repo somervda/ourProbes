78c7a382d8e17eb8d9682e758ba7d4785dbc20db
David  on Go <davidsomerville@comcast.net>
2020-01-18 11:02:15 -0500

    added 5 minute scheduler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,3 +18,12 @@ exports.usersDateCreated = functions.firestore
       { merge: true }
     );
   });
+
+exports.scheduledFunction = functions.pubsub
+  .schedule("every 5 minutes")
+  .onRun(context => {
+    // Used to maintain firestore replication of the IOT device information
+    // and load the most recent published probe results 12*24*31 invocations per month
+    console.log("This will be run every 5 minutes!");
+    return null;
+  });
