3dcdadab20f109b93b87a70ee0fb63b77d95ff4f
David <39265971+somervda@users.noreply.github.com>
2020-02-18 10:26:56 -0500

    added scheduler function

---
+++ functions/src/cronjobs.ts
@@ -0,0 +1,33 @@
+import * as functions from "firebase-functions";
+
+export const hourlyFunction = functions.pubsub
+  .schedule("0 * * * *")
+  .onRun(context => {
+    // Runs at the start of every hour
+    // run 2 jobs
+    // 1 - each hour summarize probeEvent data collected over the last hour and delete data more than 7 days old
+    // 2 - Summarize hourly data from previous day , delete hourly data more than 60 days old.
+    // Summarize to device/probe granularity
+    // Normally would use a timeseries database for this but this is easier to do for now
+    // Wrapping the 2 cron activities in one schedule function because I am cheep (first 3 scheduler function are free)
+
+    const currentTime = new Date();
+    const hour = currentTime.getHours();
+    hourlyProcess();
+    console.log("This will be run every hour!", currentTime, hour);
+    if (hour === 0) {
+      console.log("This will be run every day!", currentTime, hour);
+      dailyProcess();
+    }
+    return null;
+  });
+
+function hourlyProcess() {
+  console.log("hourlyProcess", Date.now());
+  // Read all data in a timerange and get statistics for mean, mode, max, min, counts,stddev
+}
+
+function dailyProcess() {
+  console.log("dailyProcess", Date.now());
+  // Read all data in a timerange and get statistics for mean, mode, max, min, counts,stddev
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,11 +4,4 @@ export { probeEventsOnPublish } from "./probeEvents";
 
 export { deviceCreate, deviceUpdate } from "./device";
 
-// exports.scheduledFunction = functions.pubsub
-//   .schedule("every 5 minutes")
-//   .onRun(context => {
-//     // Used to maintain firestore replication of the IOT device information
-//     // and load the most recent published probe results 12*24*31 invocations per month
-//     console.log("This will be run every 5 minutes!");
-//     return null;
-//   });
+export { hourlyFunction } from "./cronjobs";
