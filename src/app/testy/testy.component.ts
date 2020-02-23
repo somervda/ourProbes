3efd21afa4fcbf866d3fc16936ae7e07c8e804bd
David <39265971+somervda@users.noreply.github.com>
2020-02-23 15:31:27 -0500

    wip

--- src/app/testy/testy.component.ts
+++ src/app/testy/testy.component.ts
@@ -13,17 +13,46 @@ export class TestyComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     const to = new Date();
-    const from = new Date(to.getTime() - 1000 * 3600);
+    const from = new Date(to.getTime() - 1000 * 20000);
+    console.log("get data", from, to);
     this.mes$$ = this.afs
-      .collection("measurements", ref =>
+      .collection("measurementSummaries", ref =>
         ref
-          .where("UMT", ">=", from)
-          .where("UMT", "<=", to)
-          .orderBy("deviceId")
-          .limit(10)
+          .where("umt", ">=", from)
+          .where("umt", "<=", to)
+          .where("period", "==", 1)
+          .where("deviceId", "==", "D0002")
+          .where("probeId", "==", "zHc8RqIk452QxMLo0JKJ")
+          .where("type", "==", "bps")
       )
       .get()
-      .subscribe(snaps => snaps.docs.map(m => console.log(m.data())));
+      .subscribe(snaps => {
+        // Create required series from the measurementSummaries
+        const meanSeries = snaps.docs.reduce(
+          (a, s) =>
+            a.concat({
+              umt: s.data().umt.toDate(),
+              value: Math.round(s.data().mean)
+            }),
+          []
+        );
+        const p50Series = snaps.docs.reduce(
+          (a, s) =>
+            a.concat({
+              umt: s.data().umt.toDate(),
+              value: Math.round(s.data().p50)
+            }),
+          []
+        );
+        snaps.docs.map(m => console.log("Data:", m.data()));
+        console.log("meanSeries:", meanSeries);
+        console.log("p50Series:", p50Series);
+        // Build the multi series object required by the ngc-chart
+        let graphData = [];
+        graphData.push({ name: "mean", series: meanSeries });
+        graphData.push({ name: "p50", series: p50Series });
+        console.log("graphData:", graphData);
+      });
   }
 
   ngOnDestroy() {
