aec137171364650ad706873f1bf301b6be2be470
David <39265971+somervda@users.noreply.github.com>
2020-03-18 11:25:17 -0400

    comment change

--- src/probe/main.py
+++ src/probe/main.py
@@ -116,8 +116,8 @@ def create_jwt(project_id, private_key, algorithm, token_ttl):
 
 
 def get_mqtt_client(project_id, cloud_region, registry_id, device_id, jwt):
-    """Create our MQTT client. The client_id is a unique string that identifies
-    this device. For Google Cloud IoT Core, it must be in the format below."""
+    # Create our MQTT client. The client_id is a unique string that identifies
+    # this device. For Google Cloud IoT Core, it must be in the format below.
     client_id = 'projects/{}/locations/{}/registries/{}/devices/{}'.format(
         project_id, cloud_region, registry_id, device_id)
     # print('Sending message with password {}'.format(jwt))
